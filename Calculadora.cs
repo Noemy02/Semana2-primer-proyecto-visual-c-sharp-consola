1ce6ea9a4febfbdcb5c33d337db278ef7fed2c95
Lorena Noemy Bermudez Rodriguez <lorenabermudezro@gmail.com>
2022-07-29 19:29:01 -0600

    Agregación de código

--- Calculadora.cs
+++ Calculadora.cs
@@ -10,6 +10,11 @@ namespace Semana2_primer_proyecto_visual_c_sharp_consola
     {
         static void Main(string[] args)
         {
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
         }
     }
 }

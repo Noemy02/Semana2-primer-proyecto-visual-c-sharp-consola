046620ebdbed6f4043321cd7aa9f4ea7d6d77710
Lorena Noemy Bermudez Rodriguez <lorenabermudezro@gmail.com>
2022-07-29 19:26:06 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana2_primer_proyecto_visual_c_sharp_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
